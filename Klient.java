477a5a00a67a4846ab260bc92c79b87500aaa923
Hubert Bogucki <s33757@pjwstk.edu.pl>
2025-04-03 15:03:03 +0200

    napisalem podstawe Wydarzenia

--- Klient.java
+++ Klient.java
@@ -1,4 +1,10 @@
 package zad5;
 
 public class Klient {
+    private String nazwa;
+    private String nazwisko;
+    private String email;
+    //private listaRezerwacji ArrayList<Wydarzenie>
+
+
 }
--- Wydarzenie.java
+++ Wydarzenie.java
@@ -1,4 +1,33 @@
 package zad5;
 
 public class Wydarzenie {
+    private String nazwa;
+    private String data;
+    private String miejsce;
+    private int maxLiczbaMiejsc = 100;
+    private int dostepneMiejsca = 0;
+    private double cena;
+
+public Wydarzenie(String nazwa, String data, String miejsce, int maxLiczbaMiejsc, int dostepneMiejsca,double cena) {
+    this.nazwa = nazwa;
+    this.data = data;
+    this.miejsce = miejsce;
+    this.maxLiczbaMiejsc = maxLiczbaMiejsc;
+    this.dostepneMiejsca = dostepneMiejsca;
+    this.cena = cena;
+}
+
+    public String getNazwa() {return nazwa;}
+    public String getData() {return data;}
+    public String getMiejsce() {return miejsce;}
+    public int maxLiczbaMiejsc() { return maxLiczbaMiejsc;}
+    public int getDostepneMiejsca() { return dostepneMiejsca;}
+    public double getCena() {return cena;}
+
+    public void setNazwa(String nazwa) {this.nazwa = nazwa;}
+    public void setData(String data) {this.data = data;}
+    public void setMiejsce(String miejsce) {this.miejsce = miejsce;}
+    public void setMaxLiczbaMiejsc(int maxLiczbaMiejsc) {this.maxLiczbaMiejsc = maxLiczbaMiejsc;}
+    public void setDostepneMiejsca(int dostepneMiejsca) {this.dostepneMiejsca = dostepneMiejsca;}
+    public void setCena(double cena) {this.cena = cena;}
 }
