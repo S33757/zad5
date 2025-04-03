d9570122b2ca0caeb076b74d2fda6b024da6badb
Hubert Bogucki <s33757@pjwstk.edu.pl>
2025-04-03 15:08:45 +0200

    dodalem podstawe Klient

--- Klient.java
+++ Klient.java
@@ -6,5 +6,16 @@ public class Klient {
     private String email;
     //private listaRezerwacji ArrayList<Wydarzenie>
 
+public Klient(String nazwa, String nazwisko, String email) {
+    this.nazwa = nazwa;
+    this.nazwisko = nazwisko;
+    this.email = email;
+}
+    public String getNazwa() { return this.nazwa; }
+    public String getNazwisko() { return nazwisko; }
+    public String getEmail() { return email; }
 
+    public void setNazwa(String nazwa) { this.nazwa = nazwa; }
+    public void setNazwisko(String nazwisko) { this.nazwisko = nazwisko; }
+    public void setEmail(String email) { this.email = email; }
 }
